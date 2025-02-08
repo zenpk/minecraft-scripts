b356ac61ef7c6832318efda7926132535d2d959a
zenpk <99558412+zenpk@users.noreply.github.com>
2025-02-08 16:32:12 +0800

    java

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-LD_LIBRARY_PATH=. ./bedrock_server
+#!/bin/bash
+
+if [ "$1" = "fg" ]; then
+    exec java -Xms1G -Xmx4G -jar server.jar --nogui
+else
+    nohup java -Xms1G -Xmx4G -jar server.jar --nogui > .log 2>&1 &
+    echo $! > pid.txt
+fi
